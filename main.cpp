5e184137b73a2bb43f930bdc3aa3fb3a663e458b
BerniSc <bernhard.schoeffel@gmail.com>
2025-04-16 19:17:55 +0200

    Added square implementation and toggle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // main.cpp
 
+#include <cstdint>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -54,17 +55,28 @@ int main() {
     glBindVertexArray(VAO);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };  
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
 
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };  
     // Create Buffer -> Sending Data over to Graphics Card
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create EBO To share some of draw the vertices as a square using some of the same edges -> ie giving indices that can change the order drawn
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Create Shader to display the structure of the sent data 
     unsigned int vertexShader;
@@ -116,14 +128,26 @@ int main() {
     // Tell GPU how to map our Vertices (Data to Actual used Data) :)
     // 0 comes from our specifing the position0 in shadercode
     // Automagically uses the last bound VBO (Last bond to GL_ARRAY_BUFFER 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
 
+    uint32_t counter = 0;
+    bool toggle = false;
     while(!glfwWindowShouldClose(window)) {
+        if(++counter >= 1'000) {
+            if((toggle = !toggle)) {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            } else {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            }
+            counter = 0;
+        }
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        /*glDrawArrays(GL_TRIANGLES, 0, 3);*/
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*) 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
