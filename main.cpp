4bea2abcc9fc4ebcaacaf5b1f076d9be13fc6fc9
BerniSc <bernhard.schoeffel@gmail.com>
2025-04-17 20:49:03 +0200

    Implemented Rectangleclass

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // main.cpp
 
 #include <glad/glad.h>
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -30,6 +31,24 @@ void main() {
    FragColor = vec4(1.0, 0.2, 0.1, 0.7);
 }
 )glsl";
+
+const char* fragmentShaderGreenSource = R"glsl(
+#version 330 core
+out vec4 FragColor;
+void main() {
+   FragColor = vec4(0.1, 1.0, 0.4, 0.7);
+}
+)glsl";
+
+const char* fragmentShaderBlueSource = R"glsl(
+#version 330 core
+out vec4 FragColor;
+void main() {
+   FragColor = vec4(0.1, 0.4, 1.0, 0.7);
+}
+)glsl";
+
+
 
 class GLException {
     private:
@@ -162,6 +181,76 @@ class Triangle {
         }
 };
 
+class Rectangle {
+    private:
+        GLuint elementBufferObjectID = 0;
+        GLuint vertexBufferArrayID = 0;
+        GLuint vertexBufferObjectID = 0;
+        const ShaderProgram& shader;
+
+        const float* vertices;
+
+        // Our recangle consists of two triangles as OpenGL is designed to work with those
+        // As we do not want to specify unneccesary points and send them to the GPU as this 
+        // is expensive we use this approach with 4 points and then indices 
+        // We 
+        // Fallback vertices
+        static constexpr float vertices_[12] = {
+             0.5f,  0.5f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+        };
+
+        unsigned int indices[6] = {  
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+        };
+
+        void initialize() {
+            // Generate VBA as template-object and then VBO to store the Data
+            glGenVertexArrays(1, &vertexBufferArrayID);
+            glGenBuffers(1, &vertexBufferObjectID);
+            // Also create EBO to "merge" our two triangles
+            glGenBuffers(1, &elementBufferObjectID);
+
+            glBindVertexArray(vertexBufferArrayID);
+
+            // For now draw static Data -> TODO change later to allow for dynamic or stream as well
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjectID);
+            glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+            // Incices as ELEMENT_ARRAY_BUFFER
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObjectID);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(indices), indices, GL_STATIC_DRAW);
+
+            // Shader starts at address 0, VBO is tightly packed
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+            glEnableVertexAttribArray(0);
+
+            // Clean up, unbind buffer Object first, then Array
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+        }
+
+    public:
+        Rectangle(const ShaderProgram& shader, const float* customVertices = vertices_) : 
+            shader(shader), vertices(customVertices) {
+            initialize();
+        }
+
+        ~Rectangle() {
+            glDeleteVertexArrays(1, &vertexBufferArrayID);
+            glDeleteBuffers(1, &vertexBufferObjectID);
+        }
+
+        void draw() const {
+            shader.use();
+            glBindVertexArray(vertexBufferArrayID);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
+};
+
 class GLWindow {
     private:
         uint16_t width, height;
@@ -236,6 +325,8 @@ int main() {
 
         ShaderProgram shader(vertexShaderTriangleSrc, fragmentShaderOrangeSrc);
         ShaderProgram shaderRedTriangle(vertexShaderTriangleSrc, fragmentShaderRedSource);
+        ShaderProgram shaderGreen(vertexShaderTriangleSrc, fragmentShaderGreenSource);
+        ShaderProgram shaderBlue(vertexShaderTriangleSrc, fragmentShaderBlueSource);
 
         Triangle triangle(shader);
 
@@ -246,12 +337,25 @@ int main() {
              0.3f,  0.1f, 0.0f
         };
 
+        static constexpr float verticesRectSmaller[12] = {
+             0.2f,  0.2f, 0.0f,  // top right
+             0.2f, -0.2f, 0.0f,  // bottom right
+            -0.2f, -0.2f, 0.0f,  // bottom left
+            -0.2f,  0.2f, 0.0f   // top left
+        };
+
+
         Triangle triangleSmaller(shaderRedTriangle, verticesSmaller);
+
+        Rectangle rectangle(shaderGreen);
+        Rectangle rectangleBlue(shaderBlue, verticesRectSmaller);
 
         while(window.shouldKeepAlive()) {
             window.clear();
+            rectangle.draw();
             triangle.draw();
             triangleSmaller.draw();
+            rectangleBlue.draw();
             window.display();
             window.pollEvents();
         }
