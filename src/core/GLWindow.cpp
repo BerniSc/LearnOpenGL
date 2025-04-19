6b600de673918faeea1215c16511cb2789510400
BerniSc <bernhard.schoeffel@gmail.com>
2025-04-19 13:32:26 +0200

    Fixed clearcolour to be less aggressive

--- src/core/GLWindow.cpp
+++ src/core/GLWindow.cpp
@@ -40,7 +40,7 @@ bool GLWindow::shouldKeepAlive() const {
 }
 
 void GLWindow::clear() const {
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
